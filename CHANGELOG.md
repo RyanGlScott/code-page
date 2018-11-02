17379b98fc99105a495c6b7553b54f78792c5794
Ryan Scott <ryan.gl.scott@gmail.com>
2018-11-02 12:42:16 -0400

    Attempt to adjust TextEncoding on non-Windows
    If someone expects their code page to be 65001 on Windows, it seems
    sensible that the TextEncoding should be utf8 on non-Windows
    (especially since folks are asking for this behavior in
    bos/criterion#205).
    
    Unfortunately, this does require some breaking changes, since
    `withCodePage` now can error at runtime on non-Windows if supplied
    an unknown `CodePage`, and `withCodePageVerbosity` had to be renamed
    to `withCodePageOptions`. Luckily, I'm not aware of anyone who
    actually uses either function, so I think this breakage shouldn't be
    noticeable.
    
    Along the way, I did some much-needed refactoring, including:
    
    * The introduction of a `System.IO.CodePage.Internal`, modules, which
      now houses the `Options` data type, among other things.
    * Added support for the 1252 (Latin1) code page.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,24 @@
+## next [????.??.??]
+* Rename the `withCodePageVerbosity` function to `withCodePageOptions` to
+  reflect the fact that its first argument is now an `Options` data type
+  instead of just a `Bool` to represent its verbosity. (The ability to
+  configure verbosity is now controlled through the `chatty` field of
+  `Options`.)
+* On non-Windows OSes, `withCodePage` (and related functions) now make a best
+  effort guess in converting the supplied `CodePage` to a `TextEncoding` and
+  adjusing the current `TextEncoding` to that one. (For instance, `withCP65001`
+  will adjust the current `TextEncoding` to be `utf8` on non-Windows OSes.)
+  If the supplied `CodePage` does not map to a known `TextEncoding`, these
+  functions will error at runtime on non-Windows OSes.
+
+  This is a departure from the previous major version of `code-page`, where
+  these functions did not do anything at all on non-Windows OSes. If you
+  would like to recover this old behavior, use
+  `withCodePageOptions defaultOptions{nonWindowsBehavior = NonWindowsDoNothing}`.
+* Add `withCP1252` and `cp1252` for the Latin1 code page.
+* Add a `System.IO.CodePage.Internal` module that contains certain internal
+  details (such as the constructors of `Options` and `NonWindowsBehavior`).
+
 ### 0.1.3 [2017.03.15]
 * Fix the build on GHC 7.8 and older
 
--- src/System/IO/CodePage.hs
+++ src/System/IO/CodePage.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 {-|
 Module:      System.IO.CodePage
@@ -12,60 +13,52 @@ Exports functions which adjust code pages on Windows, and do nothing on other
 operating systems.
 -}
 module System.IO.CodePage (
+      -- * Adjusting 'CodePage's
       withCP65001
     , withCP1200
     , withCP1201
     , withCP12000
     , withCP12001
+    , withCP1252
     , withCodePage
-    , withCodePageVerbosity
+    , withCodePageOptions
 
+      -- * Notable 'CodePage's
     , CodePage
     , cp65001
     , cp1200
     , cp1201
     , cp12000
     , cp12001
+    , cp1252
+
+      -- * 'Options'
+    , Options
+    , defaultOptions
+      -- ** Record fields of 'Options'
+    , chatty
+    , nonWindowsBehavior
+
+      -- ** 'NonWindowsBehavior'
+    , NonWindowsBehavior
+      -- ** Constructing 'NonWindowsBehavior'
+    , nonWindowsDoNothing
+    , nonWindowsFallbackCodePageEncoding
+    , defaultFallbackCodePageEncoding
     ) where
 
+import Control.Exception (bracket_)
+import Control.Monad (when)
+import Data.Foldable (forM_)
+import System.IO ( TextEncoding, hGetEncoding, hPutStrLn, hSetEncoding
+                 , stderr, stdin, stdout )
+import System.IO.CodePage.Internal
+
 #ifdef WINDOWS
-import           Control.Exception (bracket_)
-import           Control.Monad (when)
-import           Data.Foldable (forM_)
-import           System.IO (hGetEncoding, hPutStrLn, hSetEncoding, stderr, stdin, stdout)
-import qualified System.Win32.CodePage as Win32 (CodePage)
-import           System.Win32.CodePage hiding (CodePage)
+import System.Win32.CodePage hiding (CodePage)
 #else
-import           Data.Word (Word32)
+import GHC.IO.Encoding (textEncodingName)
 #endif
-
--- | A numeric type representing Windows code pages.
-type CodePage =
-#ifdef WINDOWS
-  Win32.CodePage
-#else
-  Word32
-#endif
-
--- | The UTF-8 code page.
-cp65001 :: CodePage
-cp65001 = 65001
-
--- | The UTF-16LE code page.
-cp1200 :: CodePage
-cp1200 = 1200
-
--- | The UTF-16BE code page.
-cp1201 :: CodePage
-cp1201 = 1201
-
--- | The UTF-32LE code page.
-cp12000 :: CodePage
-cp12000 = 12000
-
--- | The UTF-32BE code page.
-cp12001 :: CodePage
-cp12001 = 12001
 
 -- | Sets the code page for an action to UTF-8 as necessary.
 withCP65001 :: IO a -> IO a
@@ -87,9 +80,20 @@ withCP12000 = withCodePage cp12000
 withCP12001 :: IO a -> IO a
 withCP12001 = withCodePage cp12001
 
+-- | Sets the code page for an action to Latin1 as necessary.
+withCP1252 :: IO a -> IO a
+withCP1252 = withCodePage cp1252
+
 -- | Sets the code page for an action as necessary.
+--
+-- On operating systems besides Windows, this will make an effort to change
+-- the current 'TextEncoding' to something that is equivalent to the supplied
+-- 'CodePage'. Currently, the only supported 'CodePage's on non-Windows OSes
+-- are 'cp65001', 'cp1200', 'cp1201', 'cp12000', and 'cp12001'. Supplying any
+-- other 'CodePage' will result in a runtime error on non-Windows OSes. (If you
+-- would like to configure this behavior, use 'withCodePageOptions' instead.)
 withCodePage :: CodePage -> IO a -> IO a
-withCodePage = withCodePageVerbosity False
+withCodePage = withCodePageOptions defaultOptions
 
 -- | Sets the code page for an action as necessary. If the 'Bool' argument is 'True',
 -- this function will emit a warning to @stderr@ indicating that the code page has
@@ -98,56 +102,86 @@ withCodePage = withCodePageVerbosity False
 -- Taken from the stack codebase
 -- (https://github.com/commercialhaskell/stack/blob/21e517ba88b3c6bee475fb00ad95f280e7285a54/src/main/Main.hs#L82-L123)
 -- which is under a 3-clause BSD license
-withCodePageVerbosity :: Bool -> CodePage -> IO a -> IO a
+withCodePageOptions :: Options -> CodePage -> IO a -> IO a
+withCodePageOptions (Options{chatty, nonWindowsBehavior}) cp inner =
+  case nonWindowsBehavior of
+    NonWindowsDoNothing -> inner
+    NonWindowsFallbackCodePageEncoding fallback -> do
 #ifdef WINDOWS
-withCodePageVerbosity chatty cp inner = do
-    origCPI <- getConsoleCP
-    origCPO <- getConsoleOutputCP
-    mbOrigStdinEnc  <- hGetEncoding stdin
-    mbOrigStdoutEnc <- hGetEncoding stdout
-    mbOrigStderrEnc <- hGetEncoding stderr
-
-    let setInput  = origCPI /= cp
-        setOutput = origCPO /= cp
-        fixInput
-            | setInput = bracket_
-                (do
-                    setConsoleCP cp
-                    hSetEncoding stdin expected
-                    )
-                (do
-                    setConsoleCP origCPI
-                    forM_ mbOrigStdinEnc $ hSetEncoding stdin
-                    )
-            | otherwise = id
-        fixOutput
-            | setOutput = bracket_
-                (do
-                    setConsoleOutputCP cp
-                    hSetEncoding stdout expected
-                    hSetEncoding stderr expected
-                    )
-                (do
-                    setConsoleOutputCP origCPO
-                    forM_ mbOrigStdoutEnc $ hSetEncoding stdout
-                    forM_ mbOrigStderrEnc $ hSetEncoding stderr
-                    )
-            | otherwise = id
-
-    case (setInput, setOutput) of
-        (False, False) -> return ()
-        (True, True) -> warn ""
-        (True, False) -> warn " input"
-        (False, True) -> warn " output"
-
-    fixInput $ fixOutput inner
-  where
-    expected = codePageEncoding cp
-    warn typ = when chatty $ hPutStrLn stderr $ concat
-        [ "Setting"
-        , typ
-        , " codepage to " ++ show cp
-        ]
+      origCPI <- getConsoleCP
+      origCPO <- getConsoleOutputCP
 #else
-withCodePageVerbosity _ _ inner = inner
+      -- These are never used on non-Windows OSes,
+      -- so their values are irrelevant
+      let origCPI = 0
+          origCPO = 0
+#endif
+      mbOrigStdinEnc  <- hGetEncoding stdin
+      mbOrigStdoutEnc <- hGetEncoding stdout
+      mbOrigStderrEnc <- hGetEncoding stderr
+
+      let expected = codePageEncoding' fallback cp
+#ifdef WINDOWS
+          setInput  = origCPI /= cp
+          setOutput = origCPO /= cp
+#else
+          -- Crude, but the best available option on non-Windows OSes
+          setInput  = fmap textEncodingName mbOrigStdinEnc
+                        /= Just (textEncodingName expected)
+          setOutput = fmap textEncodingName mbOrigStdoutEnc
+                        /= Just (textEncodingName expected)
+#endif
+          fixInput
+              | setInput = bracket_
+                  (do
+                      setConsoleCP' cp
+                      hSetEncoding stdin expected
+                      )
+                  (do
+                      setConsoleCP' origCPI
+                      forM_ mbOrigStdinEnc $ hSetEncoding stdin
+                      )
+              | otherwise = id
+          fixOutput
+              | setOutput = bracket_
+                  (do
+                      setConsoleOutputCP' cp
+                      hSetEncoding stdout expected
+                      hSetEncoding stderr expected
+                      )
+                  (do
+                      setConsoleOutputCP' origCPO
+                      forM_ mbOrigStdoutEnc $ hSetEncoding stdout
+                      forM_ mbOrigStderrEnc $ hSetEncoding stderr
+                      )
+              | otherwise = id
+
+      case (setInput, setOutput) of
+          (False, False) -> return ()
+          (True, True) -> warn ""
+          (True, False) -> warn " input"
+          (False, True) -> warn " output"
+
+      fixInput $ fixOutput inner
+    where
+      warn typ = when chatty $ hPutStrLn stderr $ concat
+          [ "Setting"
+          , typ
+          , " codepage to " ++ show cp
+          ]
+
+codePageEncoding' :: (CodePage -> TextEncoding) -> CodePage -> TextEncoding
+#ifdef WINDOWS
+codePageEncoding' _ = codePageEncoding
+#else
+codePageEncoding' = id
+#endif
+
+setConsoleCP', setConsoleOutputCP' :: CodePage -> IO ()
+#ifdef WINDOWS
+setConsoleCP'       = setConsoleCP
+setConsoleOutputCP' = setConsoleOutputCP'
+#else
+setConsoleCP'       _ = return ()
+setConsoleOutputCP' _ = return ()
 #endif
